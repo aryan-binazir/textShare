c4a22eb65de83b415c40c3f7283517e4581365d5
Aryan Binazir <65728619+aryan-binazir@users.noreply.github.com>
2023-08-18 15:49:27 -0400

    Added Routes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # textShare
 textShare: A simple platform for users to quickly share plain text, code snippets, and notes. Effortlessly share and access your text anytime, anywhere!
+
+Based on SnippetBox.
+
--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 
 // Handlers
 func home(w http.ResponseWriter, r *http.Request) {
+	// Do not allow all paths to default to "/"
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from textShare"))
 }
 
@@ -15,6 +21,12 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
@@ -25,7 +37,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on :3000")
-	err := http.ListenAndServe(":3000", mux)
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
