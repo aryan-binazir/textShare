2ff533aada51576ffd584b38d0723415972bafef
Aryan Binazir <65728619+aryan-binazir@users.noreply.github.com>
2023-08-24 14:35:33 -0400

    Added single-record SQL query

--- internal/models/textSnippets.go
+++ internal/models/textSnippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -18,11 +19,35 @@ type TextSnippetModel struct {
 }
 
 func (m *TextSnippetModel) Insert(title string, content string, expires int) (int, error) {
-	return 0, nil
+	sqlStatement := `INSERT INTO snippets (title, content, created, expires)
+    VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(sqlStatement, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (m *TextSnippetModel) Get(id int) (*TextSnippet, error) {
-	return nil, nil
+	s := &TextSnippet{}
+
+	err := m.DB.QueryRow("SELECT ...", id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return s, nil
 }
 
 func (m *TextSnippetModel) Latest() ([]*TextSnippet, error) {
